1fec37de0b7476aada4b2ddc69f9969c2644c03d
Gene Le <gene.gkl@gmail.com>
2014-04-13 20:35:49 -0500

    finish template solutions

--- src/Template.js
+++ src/Template.js
@@ -1,3 +1,24 @@
-var render = function(){
+var render = function(template, obj){
 
+  var checkForMatch = function (objectProperty, object) {
+    if ( template.match("{{" + objectProperty + "}}") ) {
+      var interpolation = "{{" + objectProperty
+        + "}}";
+      template = template.replace(interpolation, object[objectProperty]);
+      checkForMatch(objectProperty, object);
+    }
+  };
+
+  for (objProp in obj) {
+
+    // a different way of using variables in regest was a solution from stackoverlow
+    // from several years ago - which leads to the question:
+    // is the method I use not compatible with older browsers?
+    // http://stackoverflow.com/questions/3172985/javascript-use-variable-in-string-match
+
+    checkForMatch(objProp, obj);
+  }
+
+
+  return template;
 };

d6dca7bfdf1ee4ddd80b8f983645cc7c37fb30b0
Gene Le <gene.gkl@gmail.com>
2014-04-17 21:56:19 -0500

    complete music vis part 2 solutions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 
 var videos = [
-  { title: 'Get Krazy', youtubeId: 'GyR2HJ9B7aM' },
-  { title: 'Sadness', youtubeId: 'sIeJSSjTG3k' }
+  { title: 'Get Krazy', youtubeId: 'GyR2HJ9B7aM', genre: 'kid rap' },
+  { title: 'Sadness', youtubeId: 'sIeJSSjTG3k', genre: 'piano' },
+  { title: 'Drums', youtubeId: 'vhiPKAPD5Aw', genre: 'piano' },
+  { title: 'Pablo Fancisco', youtubeId: 'RLmRDfG0d1A', genre: 'comedy' }
 ];
+
+var stats = {};
 
 var renderVideoList = function () {
   // TODO
@@ -19,7 +23,38 @@ $('form').on('submit', function(e) {
   e.preventDefault();
   var title = $('.title').val();
   var youtubeId = $('.youtubeId').val();
-  videos.push({ title: title, youtubeId: youtubeId });
+  var genre = $('input[name="genre"]').val();
+
+  videos.push({ title: title, youtubeId: youtubeId, genre: genre });
 
   renderVideoList();
+  renderGenreStats();
 });
+
+var renderGenreStats = function() {
+
+  for (var i = 0; i < videos.length; i++) {
+    var genre = videos[i]["genre"];
+    if (stats[genre]) {
+      stats[genre] += 1;
+    } else {
+      stats[genre] = 1;
+    }
+  }
+
+  var genreTemplate = $('#templates .genre').html();
+
+  $('#genre-stats').empty();
+  for (var genre in stats) {
+    var genreCount = stats[genre];
+    // TODO
+    var data = { genre: genre, count: stats[genre] };
+    genreHtml = Robin.render(genreTemplate, data);
+    $('#genre-stats').append(genreHtml);
+  }
+
+  // reset stats
+  stats = {};
+  console.log('Stats for each genre: ', stats);
+};
+
